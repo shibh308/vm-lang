b4e1c6699033faa31a3f12028ac4d4b2effa30c5
shibh308 <s17097@tokyo.kosen-ac.jp>
2019-11-22 17:36:26 +0900

    [add]main

--- gen/main.go
+++ gen/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Test")
+	arg_len := len(os.Args) - 1
+	if arg_len < 1 {
+		_, _ = fmt.Fprintln(os.Stderr, "missing argument")
+		os.Exit(1)
+	}
+	readPath := os.Args[1]
+	f, err := os.Open(readPath)
+	if err != nil {
+		_, _ = fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	s := string(b)
+	writePath := "./out.scbc"
+	if arg_len >= 2 {
+		writePath = os.Args[2]
+	}
+
+	tokens := lexString(strings.TrimSpace(s))
+	root := parseTokenSlice(tokens)
+	root.generateByteCode()
+	root.writeByteCode(writePath)
 }
